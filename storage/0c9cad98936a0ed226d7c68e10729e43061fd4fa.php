4ad63227bea8b122177ee408aae04f6b1c4da1c7
uynguyen1234 <uynnph15055@fpt.edu.vn>
2021-11-04 21:09:13 +0700

    update public

--- storage/0c9cad98936a0ed226d7c68e10729e43061fd4fa.php
+++ storage/0c9cad98936a0ed226d7c68e10729e43061fd4fa.php
@@ -14,11 +14,11 @@
     <!-- Custom fonts for this template-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- <link href="./vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css"> -->
-    <link href="./public/css/adminCss/sb-admin-2.css" rel="stylesheet" type="text/css">
+    <!-- <link href="./public/css/adminCss/sb-admin-2.css" rel="stylesheet" type="text/css"> -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
     <!-- Custom styles for this template-->
-    <link href="./public/css/sb-admin-2.min.css" rel="stylesheet">
+    <!-- <link href="./public/css/sb-admin-2.min.css" rel="stylesheet"> -->
 
 </head>
 
