7d32a95cdaf49253b6c4c9f053d82e259d95df21
anhnn02 <anhnn02.it@gmail.com>
2021-11-14 23:54:19 +0700

    fix css quiz

---
+++ storage/15e5e131855539dd37eb7d28bacd82455c588a2d.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $__env->yieldContent('title', 'PT16306 project_one'); ?></title>
+    <!-- reset -->
+    <link rel="stylesheet" href="./public/css/customerCss/reset.css">
+    <!-- icon -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css">
+    <!-- css -->
+    <link rel="stylesheet" href="./public/css/customerCss/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <div class="navbar-container">
+                <div class="navbar-fluid">
+                    <div class="navbar-logo">
+                        <a href="">
+                            <img src="./public/img/images/logo-main.png" alt="" class="img-fluid img__logo">
+                        </a>
+                    </div>
+                    <div class="navbar-content">
+                        <nav class="nav__menu">
+                            <ul class="nav__list">
+                                <?php $__currentLoopData = $menu; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $key): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                <li class="nav__item"><a href="<?php echo e($key['menu_slug']); ?>" class="nav__item-link"><?php echo e($key['menu_name']); ?></a></li>
+                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                            </ul>
+                        </nav>
+                        <div class="navbar-action">
+                            <!-- <div class="navbar-action-button">
+                                <button class="action-btn btn-secondary">
+                                    Đăng nhập
+                                </button>
+                                <button class="action-btn btn-primary">
+                                    Đăng ký
+                                </button>
+                            </div> -->
+                            <div class="navbar-action-content">
+                                <button class="action-item">
+                                    <i class="fas fa-grin-stars"></i>
+                                </button>
+                                <button class="action-item">
+                                    <i class="fas fa-bell"></i>
+                                </button>
+                                <div class="account-section action-item">
+                                    <button onclick="toggleShowHide()" id="btn-acc">
+                                        <i class="fas fa-user-circle icon-account"></i>
+                                    </button>
+                                    <div id="account-list" class="content-container-acc">
+                                        <a class="account-item" href="">Thông tin tài khoản</a>
+                                        <div class="account-item account-item--mode">
+                                            <span>
+                                                Chế độ
+                                            </span>
+                                            <div class="theme-switch-wrapper">
+                                                <label class="theme-switch" for="checkbox">
+                                                    <input type="checkbox" id="checkbox" />
+                                                    <div class="slider round">
+                                                    </div>
+                                                </label>
+                                            </div>
+                                        </div>
+                                        <a class="account-item" href="">Đăng xuất</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <main>
+            <?php echo $__env->yieldContent('main_content'); ?>;
+        </main>
+    </div>
+    <script src="./public/js/customerJs/toggle.js"></script>
+    <script src="./public/js/customerJs/darkMode.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        $(function() {
+            <?php if (isset($_SESSION['error'])) { ?>
+
+                Swal.fire({
+                    icon: 'warning',
+                    title: '<?= $_SESSION['error']; ?>',
+                    timer: 3000,
+
+                })
+
+            <?php
+                unset($_SESSION['error']);
+            } elseif (isset($_SESSION['success'])) { ?>
+                Swal.fire({
+
+                    icon: 'success',
+                    title: '<?= $_SESSION['success']; ?>',
+                    showConfirmButton: false,
+                    timer: 1500
+
+                })
+
+            <?php unset($_SESSION['success']);
+            }
+            ?>
+        });
+    </script>
+</body>
+
+</html><?php /**PATH C:\xampp\htdocs\project_one\app\views/customer/layout/layout.blade.php ENDPATH**/ ?>
