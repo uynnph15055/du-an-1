ad40599545b192598ba59acb8ffbc4f67c241341
uynguyen1234 <uynnph15055@fpt.edu.vn>
2021-12-03 14:01:58 +0700

    update

--- app/controllers/frontend/introCourse.php
+++ app/controllers/frontend/introCourse.php
@@ -7,6 +7,7 @@ use App\Models\modelLesson;
 use App\Models\modelMenu;
 use App\Models\modelSubject;
 use App\Models\modelBill;
+
 class introCourse extends baseController
 {
     private $menu;
@@ -17,7 +18,7 @@ class introCourse extends baseController
 
     public function index()
     {
-    
+
 
         $subject_slug = isset($_GET['mon']) ? $_GET['mon'] : null;
         $subject = modelSubject::where("subject_slug", "=", $subject_slug)->get();
@@ -27,7 +28,6 @@ class introCourse extends baseController
         $this->render("customer.courseDetail", [
             'subject' => $subject[0],
             'lesson' => $lesson,
-            'user' => $_SESSION['user_info'][0],
             'menu' => $this->menu,
             'dataBill' => $dataBill,
         ]);
